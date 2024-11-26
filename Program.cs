5e64828d0f6e0d30a2cfe36eab6479c5fbba3054
Mykola Zaleskyi <130138695+Kalyan767Z@users.noreply.github.com>
2024-11-26 15:04:30 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_number_for_parity
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Your number: ");
+            if (double.TryParse(Console.ReadLine(), out double number))
+            {
+                if (number % 2 == 0)
+                    Console.WriteLine("Your number is even");
+                
+                else Console.WriteLine("Your number is odd");
+            }
+            else Console.WriteLine("Wrong number");
+
+        }
+    }
+}
